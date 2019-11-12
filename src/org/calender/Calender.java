b5000f0d8ad1292294eb8227651a2977e88fde6f
suchitrasr <suchi.ssr@gmail.com>
2019-11-12 15:04:03 +0530

    changed calendar....

--- src/org/calender/Calender.java
+++ src/org/calender/Calender.java
@@ -4,17 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Calender {
+	
+	public void notification(){
+		System.out.println("This is a notification from suchitra");
+	}
+	
 	public static void main(String[] args) {
 		WebDriver driver=null;
 		BaseClass obj=new BaseClass();
 		obj.browserLaunch("chrome");
 		obj.launchUrl("https://www.happyeasygo.com/flight/?utm_source=google&utm_medium=cpc&utm_campaign=GG_PC_SE_IN_en_Flight_Generic_NA_Exact&gclid=EAIaIQobChMI-9HWsO-05QIVzRErCh2UdQCCEAAYASAAEgIB1fD_BwE");
-	
-	driver.findElement(By.id("D_date")).click();
-	
-	
+		driver.findElement(By.id("D_date")).click();
 	}
-	
 	
 	
 
